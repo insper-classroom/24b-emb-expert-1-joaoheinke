f520a32dc40ef1db9cc67a7973b79a68fccee462
joaoheinke <joaovtito@gmail.com>
2024-10-31 15:38:47 +0000

    começo

--- main/main.c
+++ main/main.c
@@ -4,14 +4,23 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include "servo.h"
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/gpio.h"
 
-int main() {
-    stdio_init_all();
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+bool direction = true;
+int currentMillis = 400;
+int servoPin = 0;
+
+int main()
+{
+    setServo(servoPin, currentMillis);
+    while (true)
+    {
+        currentMillis += (direction)?5:-5;
+        if (currentMillis >= 2400) direction = false;
+        if (currentMillis <= 400) direction = true;
+        setMillis(servoPin, currentMillis);
+        sleep_ms(10);
     }
 }
